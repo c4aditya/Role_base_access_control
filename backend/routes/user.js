588c3ec5449461ef6f6e5aec2b5ae906a43acb07
Aditya Singh <singhas1418@gmail.com>
2025-04-27 16:58:33 +0530

    implementing backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,15 +1,16 @@
 const express = require("express")
-
+const { getAllPosts } = require("../controller/posts");
 const router = express.Router();
 
 // definig route handlers thise are define in controller 
 
-const signUp = require("../controller/authSignUp")
+const signUp = require("../controller/authSignUp");
 const logIn = require("../controller/authLogin");
 
 // router creaction 
 
 router.post("/login", logIn);
 router.post("/signUp", signUp)
+router.get("/allPost",getAllPosts)
  
 module.exports = router;
