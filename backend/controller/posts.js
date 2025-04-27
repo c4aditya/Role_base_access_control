b92aa3049065d1a0d46139fe1090abd60a6eb380
Aditya Singh <singhas1418@gmail.com>
2025-04-27 11:39:51 +0530

    implemented backend for all routes like update ,delete ,add, posts

--- backend/controller/posts.js
+++ backend/controller/posts.js
@@ -67,6 +67,7 @@ async function getAllPosts(req, res) {
 
 
 // geting a single post 
+
 //console.log("make entry single post ")
 async function getSinglePost(req, res) {
 
@@ -75,12 +76,15 @@ async function getSinglePost(req, res) {
     try {
         // extractig the id 
       
-        const id = req.params.id
+        const id = req.params.id;
         // extracting the items 
-        const getSinglePost = await post.findById(id );
+        const getSinglePost = await post.findById({_id : id} );
+        
+         // its only check the id formate like in mongobd id are in 24 charactrers then if the cheracters is 24 but with matching character there are no object then this if blck is run 
+       
         if (!getSinglePost) {
 
-            res.status(401).json({
+          return  res.status(401).json({
                 sucess: false,
                 message: "Not valid ID",
 
@@ -109,7 +113,84 @@ async function getSinglePost(req, res) {
 
 // dekete a singePost 
 
+console.log("outside the deleting post ")
+async function deletingPost(req , res){
+
+    console.log("inside the deleting post")
+
+    try{
+
+        const id = req.params.id;
+
+        const deletePost = await post.findByIdAndDelete({_id : id});
+ // its only check the id formate like in mongobd id are in 24 charactrers then if the cheracters is 24 but with matching character there are no object then this if blck is run 
+       
+        if(!deletePost){
+
+            res.status(401).json({
+                sucess:false,
+                message:"Id not found"
+            })
+        }
+
+        res.status(200).json({
+            sucess:true,
+            message:"Post is deleted"
+        })
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+            sucess:true,
+            message:"Delete post Now workign"
+        })
+
+    }
+}
 
 
+// update the single post 
 
-module.exports = {createPost, getAllPosts, getSinglePost};
+async function updatePost(req , res){
+
+    // for this we use find by id and update 
+    try{
+
+        const id = req.params.id;
+
+        const {title , description} = req.body
+
+        const updatePost = await post.findByIdAndUpdate({_id : id} , {title , description , createdAt : Date.now()})
+       // its only check the id formate like in mongobd id are in 24 charactrers then if the cheracters is 24 but with matching character there are no object then this if blck is run 
+
+        if(!updatePost){
+            return res.status(500).json({
+                sucess:false,
+                message:"Your Id is invalid please provide me the valid id "
+            })
+        }
+
+        res.status(200).json({
+            sucess:true,
+            data:updatePost,
+            message:`Your post is updated for this ${id} `
+        })
+
+        
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+            sucess:false,
+            message:"Update Post is not working",
+        })
+
+    }
+}
+
+
+module.exports = {createPost, getAllPosts, getSinglePost , deletingPost , updatePost};
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const veriftAdmin = require("../middlewares/authMiddleware")
-const {createPost , getAllPosts , getSinglePost} = require("../controller/posts")
+const {createPost , getAllPosts , getSinglePost ,deletingPost,updatePost} = require("../controller/posts")
 const router = express.Router();
 
 const admin = require("../controller/authAdmin");
@@ -10,7 +10,8 @@ router.post("/adminLogin" , admin);
 router.post("/createPost", veriftAdmin , createPost);
 router.get("/getAllPosts", veriftAdmin , getAllPosts);
 router.get("/singlePost/:id" , veriftAdmin ,getSinglePost);
-
+router.delete("/deletePost/:id" , veriftAdmin  , deletingPost);
+router.put("/updatePost/:id", veriftAdmin , updatePost)
 
 
 module.exports = router;
