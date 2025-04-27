b21f6612cc9bd90de1c4ffe2b5577dcb14e7af73
Aditya Singh <singhas1418@gmail.com>
2025-04-27 10:36:43 +0530

    adding controller for get single post

--- backend/controller/posts.js
+++ backend/controller/posts.js
@@ -2,62 +2,63 @@ const post = require("../model/post");
 
 
 
-async function createPost (req,res){
+async function createPost(req, res) {
 
-    try{
+    try {
 
-        const {title ,description} = req.body
+        const { title, description } = req.body
 
         // databse intrection 
-        const newPost  = await  post.create({
+        const newPost = await post.create({
             title,
             description,
-        
-        }) 
+
+        })
 
         res.status(200).json({
-            sucess:true,
-            message:"Post is created"
+            sucess: true,
+            message: "Post is created"
         })
     }
 
-    catch(error){
+    catch (error) {
         console.log(error)
         res.status(500).json({
-            sucess:true,
-            message:"geeting error while post"
+            sucess: true,
+            message: "geeting error while post"
         })
 
-        
+
 
     }
 
 }
 
-// for updating the post 
-async function getAllPosts(req ,res){
+// for geeting all  the post 
 
-    try{
+async function getAllPosts(req, res) {
+
+    try {
 
         // get all posts 
 
         const getAllPost = await post.find({});
 
         res.status(200).json({
-            sucess:true,
-            data:getAllPost,
-            message:"All post get Sucessfully"
+            sucess: true,
+            data: getAllPost,
+            message: "All post get Sucessfully"
         })
-       
+
 
     }
 
-    catch(error){
+    catch (error) {
 
         res.status(500).json({
-            sucess:false,
-            data:error,
-            message:"Geeting Error while fetching all the posts "
+            sucess: false,
+            data: error,
+            message: "Geeting Error while fetching all the posts "
         })
 
     }
@@ -65,5 +66,50 @@ async function getAllPosts(req ,res){
 }
 
 
+// geting a single post 
+//console.log("make entry single post ")
+async function getSinglePost(req, res) {
 
-module.exports = {createPost , getAllPosts};
+    console.log("Inside the getSingle post");
+  
+    try {
+        // extractig the id 
+      
+        const id = req.params.id
+        // extracting the items 
+        const getSinglePost = await post.findById(id );
+        if (!getSinglePost) {
+
+            res.status(401).json({
+                sucess: false,
+                message: "Not valid ID",
+
+            })
+        }
+        res.status(200).json({
+            sucess: true,
+            data: getSinglePost,
+            message: "Geting single Post with matching ID"
+        })
+    }
+
+    catch (error) {
+
+        res.status(500).json({
+            sucess: false,
+            data: error,
+            message: "Getting error while Fetching Single Post"
+        })
+
+    }
+}
+
+// console.log("last line")
+
+
+// dekete a singePost 
+
+
+
+
+module.exports = {createPost, getAllPosts, getSinglePost};
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const veriftAdmin = require("../middlewares/authMiddleware")
-const {createPost , getAllPosts} = require("../controller/posts")
+const {createPost , getAllPosts , getSinglePost} = require("../controller/posts")
 const router = express.Router();
 
 const admin = require("../controller/authAdmin");
@@ -9,6 +9,7 @@ router.post("/adminLogin" , admin);
 
 router.post("/createPost", veriftAdmin , createPost);
 router.get("/getAllPosts", veriftAdmin , getAllPosts);
+router.get("/singlePost/:id" , veriftAdmin ,getSinglePost);
 
 
 
